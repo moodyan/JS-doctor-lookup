6b7d5deccef53de0de3b7b597609cbab68b93ce1
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-06-30 11:36:44 -0700

    Get click function to show info on doctor. Need to sort out ids.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -4,15 +4,15 @@ var displayDoctors = function(foundDoctors) {
   foundDoctors.forEach(function(doctor) {
     $('#show-doctors').append('<li class="doctor-info" npi=' + doctor.npi +'>' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</li>');
   });
-  addClick(npi)
+  addClick(npi);
 };
 
-var displayDoctorInfo = function(firstName, lastName, title, gender, bio, currentPractice, phone, image) {
-    $('#show-doctor-info').append(image + '<br>');
+var displayDoctorInfo = function(firstName, lastName, title, gender, bio, currentPractice, phone, image, npi) {
+    $('#show-doctor-info').append('<img src="' + image + '"><br>');
     $('#show-doctor-info').append('<h2>' + firstName + ' ' + lastName + ', ' + title + '</h2><hr>');
     $('#show-doctor-info').append('<h4><strong>' + gender + '</strong></h4>');
-    $('#show-doctor-info').append('<h3> Current Practice: ' + currentPractice + '<br>Phone: ' + phone + '</h3>');
-    $('#show-doctor-info').append('<h4>' + bio + '</h4>');
+    $('#show-doctor-info').append('<h3> Current Practice: </h3><h4>' + currentPractice + '</h4><h3>Phone: </h3><h4>' + phone + '</h4>');
+    $('#show-doctor-info').append('<p>' + bio + '</p>');
 };
 
 $(document).ready(function() {
@@ -31,7 +31,9 @@ $(document).ready(function() {
 var addClick = function(npi) {
   var doctor = new Doctor();
   $('.doctor-info').click(function() {
-    npi = $(this[0].npi);
+    // console.log("before: "+npi);
+    // npi = $((this)[0].npi);
+    // console.log("after: "+npi);
     doctor.getDoctorInfo(npi, displayDoctorInfo);
   });
 };
