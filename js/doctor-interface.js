f80cd1de171a5e4f6723e331645b72c8077b2581
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-06-30 10:40:15 -0700

    Add displayDoctorInfo function so that user can click on doctor name to see more info (in progress).

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -2,17 +2,26 @@ var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctors = function(foundDoctors) {
   foundDoctors.forEach(function(doctor) {
-    $('#show-doctors').append('<li>' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</li>');
+    $('#show-doctors').append('<li class="doctor-info" npi=' + doctor.npi +'>' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</li>');
   });
+  addClick(npi)
+};
+
+var displayDoctorInfo = function(firstName, lastName, title, gender, bio, currentPractice, phone, image) {
+    $('#show-doctor-info').append(image + '<br>');
+    $('#show-doctor-info').append('<h2>' + firstName + ' ' + lastName + ', ' + title + '</h2><hr>');
+    $('#show-doctor-info').append('<h4><strong>' + gender + '</strong></h4>');
+    $('#show-doctor-info').append('<h3> Current Practice: ' + currentPractice + '<br>Phone: ' + phone + '</h3>');
+    $('#show-doctor-info').append('<h4>' + bio + '</h4>');
 };
 
 $(document).ready(function() {
-  // var foundDoctors = [];
   var doctor = new Doctor();
   $('#doctor-search').submit(function(event) {
     event.preventDefault();
     var medicalIssue = $('#medical-issue').val();
     // $('#location').val("");
     doctor.getDoctors(medicalIssue, displayDoctors);
+    $('#show-doctors').empty();
   });
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,6 +1,6 @@
 var apiKey = require('./../.env').apiKey;
 
-Doctor = function(firstName, lastName, title, gender, bio, currentPractice, phone, image){
+Doctor = function(firstName, lastName, title, gender, bio, currentPractice, phone, image, npi){
   this.firstName = firstName;
   this.lastName = lastName;
   this.title = title;
@@ -9,16 +9,17 @@ Doctor = function(firstName, lastName, title, gender, bio, currentPractice, phon
   this.practice = currentPractice;
   this.phone = phone;
   this.image = image;
+  this.npi = npi;
 };
 
 Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   var foundDoctors = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
     console.log(response);
-    console.log(response.data[0].practices[0].phones[0].number);
+    console.log(image=response.data[0].npi);
     //CREATE SECOND FOR LOOP TO ACCESS ALL PRACTICES AND PHONES
     for (var i = 0; i < response.data.length; i++) {
-      var newDoctor = new Doctor(firstName=response.data[i].profile.first_name, lastName=response.data[i].profile.last_name, title=response.data[i].profile.title, gender=response.data[i].profile.gender, bio=response.data[i].profile.bio, practice=response.data[i].practices[0].name, phone=response.data[i].practices[0].phones[0].number, image=response.data[i].profile.image_url);
+      var newDoctor = new Doctor(firstName=response.data[i].profile.first_name, lastName=response.data[i].profile.last_name, title=response.data[i].profile.title, gender=response.data[i].profile.gender, bio=response.data[i].profile.bio, practice=response.data[i].practices[0].name, phone=response.data[i].practices[0].phones[0].number, image=response.data[i].profile.image_url, npi=response.data[i].npi);
       foundDoctors.push(newDoctor);
     }
     displayDoctors(foundDoctors);
