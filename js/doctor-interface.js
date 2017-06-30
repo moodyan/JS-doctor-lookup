a0448413c64c54a336e6c0e1ac1258538317c973
Alyssa Moody <alyssanicholemoody@gmail.com>
2017-06-30 10:11:32 -0700

    Add logic to backend and interfact for search by medical issue to display doctors by name that match medical issue.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,1 +1,18 @@
-var apiKey = require('./../.env').apiKey;
+var Doctor = require('./../js/doctor.js').doctorModule;
+
+var displayDoctors = function(foundDoctors) {
+  foundDoctors.forEach(function(doctor) {
+    $('#show-doctors').append('<li>' + doctor.firstName + ' ' + doctor.lastName + ', ' + doctor.title + '</li>');
+  });
+};
+
+$(document).ready(function() {
+  // var foundDoctors = [];
+  var doctor = new Doctor();
+  $('#doctor-search').submit(function(event) {
+    event.preventDefault();
+    var medicalIssue = $('#medical-issue').val();
+    // $('#location').val("");
+    doctor.getDoctors(medicalIssue, displayDoctors);
+  });
+});
---
+++ js/doctor.js
@@ -0,0 +1,30 @@
+var apiKey = require('./../.env').apiKey;
+
+Doctor = function(firstName, lastName, title, gender, bio, currentPractice, phone, image){
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.title = title;
+  this.gender = gender;
+  this.bio = bio;
+  this.practice = currentPractice;
+  this.phone = phone;
+  this.image = image;
+};
+
+Doctor.prototype.getDoctors = function(medicalIssue, displayDoctors) {
+  var foundDoctors = [];
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(response) {
+    console.log(response);
+    console.log(response.data[0].practices[0].phones[0].number);
+    //CREATE SECOND FOR LOOP TO ACCESS ALL PRACTICES AND PHONES
+    for (var i = 0; i < response.data.length; i++) {
+      var newDoctor = new Doctor(firstName=response.data[i].profile.first_name, lastName=response.data[i].profile.last_name, title=response.data[i].profile.title, gender=response.data[i].profile.gender, bio=response.data[i].profile.bio, practice=response.data[i].practices[0].name, phone=response.data[i].practices[0].phones[0].number, image=response.data[i].profile.image_url);
+      foundDoctors.push(newDoctor);
+    }
+    displayDoctors(foundDoctors);
+  }).fail(function(error) {
+    $('#show-doctors').text("fail");
+  });
+};
+
+exports.doctorModule = Doctor;
